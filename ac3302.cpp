2096b29395f4d3a2305ed0d0919952d2e6133dd0
Sputnik <2431916178@qq.com>
2025-10-22 16:00:35 +0800

    10.22

---
+++ ac3302.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+const int N=1e5+10;
+char st[N],tt;//tt表示栈顶
+string s;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    //中缀转后缀
+    cin>>s;
+    for(char c:s){//遍历字符串
+        
+    }
+    return 0;
+}
