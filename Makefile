a4e996a5cdb9627c5951d8b0b1bda72b31dbd3ce
glance <2431916178@qq.com>
2025-11-10 15:29:04 +0800

    11.10

--- Makefile
+++
@@ -1,9 +0,0 @@
-CXX = g++
-CXXFLAGS = -Wall -std=c++11
-
-# 正确的目标规则：从 .cpp 生成可执行文件
-%: %.cpp
-	$(CXX) $(CXXFLAGS) $< -o $@
-
-clean:
-	del *.exe 2>nul || true
---
+++ delete.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    //for循环
+    for(int i=0;i<100;i++){
+        printf("哈基涂");
+    }
+    int p=100;
+    while(p--)
+        printf("我永远会看着你！！");
+    return 0;
+}
