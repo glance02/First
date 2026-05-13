de6a0b57388aac69fd4994488f8cb825c901945f
glance <2431916178@qq.com>
2026-05-13 18:09:31 +0800

    完成vr报告模板

--- latex.ps1
+++ latex.ps1
@@ -1,2 +1,2 @@
-xelatex -interaction=nonstopmode -synctex=1 'kzllyy template20181214.tex'
-xelatex -interaction=nonstopmode -synctex=1 'kzllyy template20181214.tex'
+xelatex -interaction=nonstopmode -synctex=1 main.tex
+xelatex -interaction=nonstopmode -synctex=1 main.tex
