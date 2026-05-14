70e7ebc8d071ba5e1f84554307214e9ee0118ebd
glance <2431916178@qq.com>
2026-05-14 23:03:30 +0800

    添加脸部识别代码

--- latex.ps1
+++ latex.ps1
@@ -1,2 +1,2 @@
-xelatex -interaction=nonstopmode -synctex=1 main.tex
-xelatex -interaction=nonstopmode -synctex=1 main.tex
+xelatex -shell-escape -interaction=nonstopmode -synctex=1 main.tex
+xelatex -shell-escape -interaction=nonstopmode -synctex=1 main.tex
